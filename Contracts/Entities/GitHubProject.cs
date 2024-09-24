0cdf739efd5af0fd9dcbf92af7c4924129d66ddc
Ilya Grigorev <abrailia3@gmail.com>
2024-09-25 00:38:38 +0300

    GitHub entity and service

---
+++ Contracts/Entities/GitHubProject.cs
@@ -0,0 +1,19 @@
+﻿using Contracts.Interfaces;
+
+namespace Contracts.Entities;
+
+public class GitHubProject
+{
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// https://github.com/[UrlPostfix]
+    /// </summary>
+    public string UrlPostfix { get; set; }
+
+    public WebFrameworks WebFramework { get; set; }
+
+    public int Stars { get; set; }
+
+    public DateTime CreationDate { get; set; }
+}
---
+++ ProjectsLoader/Services/GitHubService.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjectsLoader.Services;
+
+public class GitHubService
+{
+    // get by id
+    // get all [with filter, for now just by framework]
+    // get meta info by url without loading
+    // save by url (download and save meta info in database)
+    // save by entity (just save metainfo without loading)
+    // update by url (sent request, if something changed on github -> update it in database)
+    // delete if loaded from disk space, not from database
+}
