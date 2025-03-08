b1fe1a5a627556f5d59a9a5c3edce76e1e0b7e96
Molotooff <kolljei@gmail.com>
2025-03-08 23:34:57 +0300

    feat: Transferring the file download function to ProjectLoader from ProjectPlanner.

--- ProjectLoader/Program.cs
+++ ProjectLoader/Program.cs
@@ -1,7 +1,36 @@
-using ProjectLoader;
+using ProjectLoader.Services;
+using Serilog;
+using StackExchange.Redis;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddSerilog(lc => lc
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.File(Path.Join(builder.Environment.ContentRootPath, "logs", "ProjectLoader.log")));
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+    ConnectionMultiplexer.Connect(sp.GetRequiredService<IConfiguration>().GetValue<string>("Redis:Queue")));
+
+builder.Services.AddSingleton<Func<string, IConnectionMultiplexer>>(sp => name =>
+{
+    var connections = sp.GetServices<IConnectionMultiplexer>().ToList();
+    return name switch
+    {
+        "queue" => connections[0],
+        _ => throw new ArgumentException("Unknown Redis connection name")
+    };
+});
+
+builder.Services.AddHostedService<ProjectLoaderService>();
+builder.Services.AddHttpClient();
 
 var host = builder.Build();
 host.Run();
